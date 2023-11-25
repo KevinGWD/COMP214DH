4f060a4cbbe48be9adc9406070aea89d06ee483e
kevinGWD <kevingwd@hotmail.com>
2023-11-24 23:13:55 -0500

    removed extra files

--- server/models/Categories.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-		 const CategoriesSchema = new mongoose.Schema({
-      name: {
-        type: String,
-        trim: true,
-        required: 'Name is required'
-        }})
-   module.exports = mongoose.model('Categories', CategoriesSchema);
