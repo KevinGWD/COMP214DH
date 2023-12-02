e4bc2a4a7e7ef447a704648841d0832134fdf880
kevinGWD <kevingwd@hotmail.com>
2023-12-02 18:21:40 -0500

    update code

--- client/src/assets/staffs/Mystaffs.jsx
+++ client/src/assets/staffs/Mystaffs.jsx
@@ -5,76 +5,67 @@ import './Mystaffs.css'
 
 export default function Mystaffs() {
 
-  const [staffs, setStaffs]=useState([])
+  const [staffs, setStaffs] = useState([])
 
-  React.useEffect(()=>{
-    axios('http://localhost:8081/api/dhstaff/').then(response=>{
-      if (response.status===200){
-        const fetchedData=response.data;
+  React.useEffect(() => {
+    axios('/api/dhstaff/').then(response => {
+      if (response.status === 200) {
+        const fetchedData = response.data;
         setStaffs(fetchedData);
         console.log(fetchedData)
       }
-   }).catch(err=>{
-    setStaffs([])
-   })  
-  },[])
+    }).catch(err => {
+      setStaffs([])
+    })
+  }, [])
 
-  const handleDelete=async (staffno)=>{
-    let arr=staffs.filter(function(obj){
-      return obj.STAFFNO!=staffno
+  const handleDelete = async (staffno) => {
+    let arr = staffs.filter(function (obj) {
+      return obj.STAFFNO != staffno
     })
     setStaffs(arr)
-    await axios.delete(`/api/dhstaff/delete`,{ params: { id: staffno }})
-   .then(()=>{
-   }
-    ).catch(err=>{
-     console.log('failed');
-   }) 
+    await axios.delete(`/api/dhstaff/delete`, { params: { id: staffno } })
+      .then(() => {
+      }
+      ).catch(err => {
+        console.log('failed');
+      })
   }
 
   return (
     <>
-    <div><h1>Staff List </h1>
-      <span><NavLink to='/loggedin/hirenewstaff' ><button>Hire New Staff</button></NavLink></span>
-    </div>
-    <br /><br /> 
-    {staffs.length===0?<h2>&nbsp;&nbsp;&nbsp;&nbsp;Can not connect to database at the moment, please try again later... ...</h2>:
-    <table>
-    <thead>
-      <tr>
-    <th>Staff No</th>
-    <th>Firstname</th>
-    <th>Lastname</th>
-    <th>Position</th>
-    <th>Sex</th>
-    <th>DOB</th>
-    <th>Salary</th>
-    <th>Branch No</th>
-    <th>Telephon</th>
-    <th>Moblie</th>
-    <th>Email</th>
-    </tr>
-    </thead>
-    <tbody>
-      {staffs.map((staff,index)=>(
-            <tr key={index}>
-            <td>{staff.STAFFNO}</td>
-            <td>{staff.FNAME}</td>
-            <td>{staff.LNAME}</td>
-            <td>{staff.POSITION}</td>
-            <td>{staff.SEX}</td>
-            <td>{staff.DOB.substring(0,10)}</td>
-            <td>{staff.SALARY}</td>
-            <td>{staff.BRANCHNO}</td>
-            <td>{staff.TELEPHONE}</td>
-            <td>{staff.MOBILE}</td>
-            <td>{staff.EMAIL}</td>
-            <td><NavLink to={{pathname:`/loggedin/item/${staff.STAFFNO}/${staff.FNAME}/${staff.LNAME}/${staff.EMAIL}/${staff.TELEPHONE}/${staff.SALARY}`}}><button className='inlineButton'>UPDATE</button></NavLink></td>
-            <td><button className='inlineButton' onClick={()=>handleDelete(staff.STAFFNO)}>DELETE</button></td> 
-            </tr>
-      ))}
-    </tbody>
-    </table>}
+      <div><h1>Staff List </h1>
+        <span><NavLink to='/loggedin/hirenewstaff' ><button>Hire New Staff</button></NavLink></span>
+      </div>
+      <br /><br />
+      {staffs.length === 0 ? <h2>&nbsp;&nbsp;&nbsp;&nbsp;Loading ... ...</h2> :
+        <table>
+          <thead>
+            {/* <tr> */}
+              <th>Staff No</th><th>Firstname</th><th>Lastname</th><th>Position</th><th>Sex</th><th>DOB</th><th>Salary</th><th>Branch No</th><th>Telephon</th><th>Moblie</th><th>Email</th>
+            {/* </tr> */}
+        </thead>
+          <tbody>
+            {staffs.map((staff, index) => (
+              <tr key={index}>
+                <td>{staff.STAFFNO}</td>
+                <td>{staff.FNAME}</td>
+                <td>{staff.LNAME}</td>
+                <td>{staff.POSITION}</td>
+                <td>{staff.SEX}</td>
+                <td>{staff.DOB.substring(0, 10)}</td>
+                <td>{staff.SALARY}</td>
+                <td>{staff.BRANCHNO}</td>
+                <td>{staff.TELEPHONE}</td>
+                <td>{staff.MOBILE}</td>
+                <td>{staff.EMAIL}</td>
+                {/* <td><NavLink to={{ pathname: `/loggedin/item/${staff.STAFFNO}/${staff.FNAME}/${staff.LNAME}/${staff.EMAIL}/${staff.TELEPHONE}/${staff.SALARY}` }}><button className='inlineButton'>UPDATE</button></NavLink></td> */}
+                <td><NavLink to={`/loggedin/item/${staff.STAFFNO}/${staff.FNAME}/${staff.LNAME}/${staff.EMAIL}/${staff.TELEPHONE}/${staff.SALARY}`} ><button className='inlineButton'>UPDATE</button></NavLink></td>
+                <td><button className='inlineButton' onClick={() => handleDelete(staff.STAFFNO)}>DELETE</button></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>}
     </>
   )
 }
--- client/src/assets/staffs/SingleStaffPage.jsx
+++ client/src/assets/staffs/SingleStaffPage.jsx
@@ -1,55 +1,59 @@
-import React, { useState , useContext, useEffect} from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import { useParams } from 'react-router'
 import { NavLink } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
+
 import Axios from 'axios'
 
 export default function SingleStaffPage() {
 
-     const{staffno, fname, lname, email, phone, salary}=useParams()
+  const navigate = useNavigate()
 
-       const [inform, setInform]=useState({email:email, phone:phone, salary:salary, staffno:staffno,submitted:false})
+  const { staffno, fname, lname, email, phone, salary } = useParams()
 
-       let errormessage='';
+  const [inform, setInform] = useState({ email: email, phone: phone, salary: salary, staffno: staffno, submitted: false })
 
-        const handleSubmit=()=>{
-          console.log("updating")
-          setInform({...inform, submitted:true})
-            Axios.put('/api/dhstaff/update',inform)
-           .then(inform.submitted==true).catch(err=>{
-             console.log('failed');
-             console.log("updated")
-           }) 
-          
-             alert(`${fname} ${lname}'s information has been updated`)
-           }
+  let errormessage = '';
 
-          
-       // }
-      useEffect(
+  const handleSubmit = () => {
+    console.log("updating")
+    Axios.put('/api/dhstaff/update', inform)
+      .then(setInform({ ...inform, submitted: true })).catch(err => {
+        console.log('failed');
+        console.log("updated")
+      })
 
-      ()=>{
-        setInform({...inform, submitted:false})
-    
-    },[inform.email,inform.phone,inform.salary])
+    //  alert(`${fname} ${lname}'s information has been updated`)
+    setTimeout(() => {
+      navigate("/loggedin")
+    }, 1000)
+  }
+  useEffect(
+
+    () => {
+      setInform({ ...inform, submitted: false })
+
+    }, [inform.email, inform.phone, inform.salary])
 
 
 
   return (
     <div>
       <h2>Updating {fname} {lname}'s information:</h2><br />
-      
+
       <form action="">{/* need submit data */}
-        <input type="text" value={inform.staffno} style={{display:'none'}}/>
+        <input type="text" value={inform.staffno} style={{ display: 'none' }} />
         <label htmlFor="email" className="update-form-input">Email</label>
-        <input type="text" value={inform.email} onChange={(e)=>{setInform({...inform, email:e.target.value})}}/><br />
+        <input type="text" value={inform.email} onChange={(e) => { setInform({ ...inform, email: e.target.value }) }} /><br />
         <label htmlFor="phone" className="update-form-input">Phone</label>
-        <input type="text" value={inform.phone} onChange={(e)=>{setInform({...inform, phone:e.target.value})}}/><br />
+        <input type="text" value={inform.phone} onChange={(e) => { setInform({ ...inform, phone: e.target.value }) }} /><br />
         <label htmlFor="salary" className="update-form-input">Salary</label>
-        <input type="number" value={inform.salary} onChange={(e)=>{setInform({...inform, salary:e.target.value})}}/><br /><br />
-        <button type='button' onClick={handleSubmit}>Save</button>{inform.submitted==true? <pre style={{color:'green', fontFamily:'bold'}}>INFORMATION HAS BEEN UPDATED</pre>:<pre style={{color:'red'}}>Hit 'Save' to update</pre>}
+        <input type="number" value={inform.salary} onChange={(e) => { setInform({ ...inform, salary: e.target.value }) }} /><br /><br />
+        <div><button type='button' onClick={handleSubmit}>Save</button></div>
       </form>
-        <br />
-        <NavLink to='/loggedin/mystaffs'><div>Back to employees list</div></NavLink>
+      <div className='reminder'>{inform.submitted == true ? <p style={{ color: 'green', fontFamily: 'bold' }}>&nbsp;&nbsp;&nbsp;INFORMATION HAS BEEN UPDATED</p> : <pre style={{ color: 'red' }}>&nbsp;&nbsp;&nbsp;Hit 'Save' to update</pre>}</div>
+      <br />
+      <NavLink to='/loggedin/mystaffs'><div>Back to employees list</div></NavLink>
 
     </div>
   )
