ebd05ec604f0772049ee8f7f4b648103228c93fb
Coutre <147004409+Coutre@users.noreply.github.com>
2023-12-08 19:37:36 -0500

    Update BranchMainMenu.jsx

--- client/src/assets/branches/BranchMainMenu.jsx
+++ client/src/assets/branches/BranchMainMenu.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import BranchSearch from './BranchSearch.jsx';
+import BranchList from './BranchResult.jsx';
+import BranchOpen from './BranchOpen.jsx';
+import MainMenu from './MainMenu.jsx';
 
 export default function BranchMainMenu() {
   return (
-    <div>Branch Main Menu</div>
-  )
+    <Router>
+      <Routes>
+        <Route path = "/Branch" element={<><MainMenu/></>}/>
+        <Route path = "/Branch/Search" element={<><BranchSearch/></>}/>
+        <Route path = "/Branch/List" element={<><BranchList/></>}/>
+        <Route path = "/Branch/Open" element={<><BranchOpen/></>}/>
+      </Routes>
+    </Router>
+  );
 }
